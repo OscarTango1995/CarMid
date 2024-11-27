OscarTango1995/Car-MID

GPS moduel & Average support Completed

include/average.h
@@ -1,0 +1,19 @@
+#ifndef AVERAGE_H
+#define AVERAGE_H
+#include <Arduino.h>
+
+struct Average
+{
+    float average;
+    int distance;
+    float fuel;
+    float dte;
+};
+
+Average calculateAverageFuelConsumption();
+void logAverageData(float fuelFlow, double distance);
+bool isArrayFull();
+void resetArray();
+Average resetTrip();
+
+#endif // AVERAGE_H

OscarTango1995/CarMid

include/average.h
@@ -1,0 +1,20 @@
+#ifndef AVERAGE_H
+#define AVERAGE_H
+#include <Arduino.h>
+
+struct Average
+{
+    float average;
+    float distanceTraveled;
+    float fuelUsed;
+    float fuelRemaining;
+    float dte;
+};
+
+Average calculateAverageFuelConsumption();
+void logAverageData(float fuelFlow, double distance);
+bool isArrayFull();
+void resetArray();
+Average resetTrip();
+
+#endif // AVERAGE_H